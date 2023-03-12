c06c161587e86026e3720782a18aa43fe7238692
SensSebastianSchlosser <sebastian.schlosser@sens.at>
2023-03-12 17:36:59 +0100

    fix testing

--- src/components/Scanner/Scanner.jsx
+++ src/components/Scanner/Scanner.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Navigate } from 'react-router-dom'
+// import { Navigate } from 'react-router-dom'
 import { BrowserMultiFormatReader } from '@zxing/library'
 
 export default class Scanner extends Component {
