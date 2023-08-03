f6b9beb39e8d50a6b64745688e44768e784acad1
SensSebastianSchlosser <117441229+SensSebastianSchlosser@users.noreply.github.com>
2023-08-03 10:21:05 +0200

    Update DeviceRow.jsx
    Fix installation_place2

--- src/components/DeviceRow.jsx
+++ src/components/DeviceRow.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-solid-svg-icons'
-import { Tooltip } from '@material-tailwind/react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import { Tooltip } from "@material-tailwind/react";
 
 /**
  * DeviceRow
@@ -10,141 +10,149 @@ import { Tooltip } from '@material-tailwind/react'
  * device: Object
  */
 export default class DeviceRow extends Component {
-	attr = this.props.device.attributes
+	attr = this.props.device.attributes;
 
 	lastData = () => {
-		var timestamp = this.attr.last_timestamp
-		if (timestamp === '0' || timestamp === '' || timestamp === null)
-			return 'nodata'
-		return new Date(timestamp).toLocaleString()
-	}
+		var timestamp = this.attr.last_timestamp;
+		if (timestamp === "0" || timestamp === "" || timestamp === null)
+			return "nodata";
+		return new Date(timestamp).toLocaleString();
+	};
 
 	checkerUndefined = (a) => {
-		return a === '0' || a === null || a === ''
-	}
+		return a === "0" || a === null || a === "";
+	};
 
 	additionalData = (attr, device) => {
 		if (device.typeId === 1) {
-			return attr.mac
+			return attr.mac;
 		}
 		if (this.checkerUndefined(attr.installation_place)) {
-			return ''
+			return "";
 		}
-		return attr.installation_place + ((attr.installation_number !== null && typeof attr.installtion_number !== 'undefined') ? (' ' + attr.installation_number) : '')
-	}
+		return (
+			attr.installation_place +
+			(attr.installation_number !== null &&
+			typeof attr.installtion_number !== "undefined"
+				? " " + attr.installation_number
+				: "") +
+			(attr.installation_place === "Sonstiges" ||
+			this.checkerUndefined(attr.installation_place)
+				? " | " + attr.installation_place2
+				: "")
+		);
+	};
 
 	comment = (attr, device) => {
 		if (device.typeId === 1) {
 			return (
-				<div className="text-sm italic mx-6 text-center">
+				<div className="mx-6 text-center text-sm italic">
 					{attr.comment}
 					{!this.checkerUndefined(attr.installation_place) &&
-						' | ' + attr.installation_place}
-					{(attr.installation_place === 'Sonstiges' || this.checkerUndefined(attr.installation_place)) &&
-						' | ' + attr.installation_place2}
+						" | " + attr.installation_place}
 				</div>
-			)
+			);
 		}
 		if (!this.checkerUndefined(attr.comment)) {
 			return (
-				<div className="text-sm italic mx-6 text-center">
+				<div className="mx-6 text-center text-sm italic">
 					{attr.comment}
 				</div>
-			)
+			);
 		}
-		return ''
-	}
+		return "";
+	};
 
 	status = (attr) => {
-		var size = 'h-10 '
-		var color = ''
+		var size = "h-10 ";
+		var color = "";
 		// farbiger Punkt
 		if (
 			attr.app_color !== null &&
-			typeof attr.app_color !== 'undefined' &&
+			typeof attr.app_color !== "undefined" &&
 			attr.app_status !== null
 		) {
 			switch (attr.app_color) {
-				case '1':
-					color = 'text-green-400'
-					break
-				case '2':
-					color = 'text-red-400'
-					break
-				case '3':
-					color = 'text-yellow-400'
-					break
-				case '4':
-					color = 'text-purple-400'
-					break
-				case '5':
-					color = 'text-orange-400'
-					break
+				case "1":
+					color = "text-green-400";
+					break;
+				case "2":
+					color = "text-red-400";
+					break;
+				case "3":
+					color = "text-yellow-400";
+					break;
+				case "4":
+					color = "text-purple-400";
+					break;
+				case "5":
+					color = "text-orange-400";
+					break;
 				default:
-					color = 'text-gray-400'
-					break
+					color = "text-gray-400";
+					break;
 			}
 			return (
 				<Tooltip content={attr.app_status}>
-					<div className="flex justify-center items-center p-2">
+					<div className="flex items-center justify-center p-2">
 						<FontAwesomeIcon
 							icon={faCircle}
 							className={size + color}
 						/>
 					</div>
 				</Tooltip>
-			)
+			);
 		}
 		// Grauer Punkt
 		if (
 			(attr.app_color === null ||
-				typeof attr.app_color === 'undefined') &&
+				typeof attr.app_color === "undefined") &&
 			attr.app_status === null
 		) {
 			return (
 				<Tooltip content="nodata">
-					<div className="flex justify-center items-center p-2">
+					<div className="flex items-center justify-center p-2">
 						<FontAwesomeIcon
 							icon={faCircle}
-							className={size + ' text-gray-400'}
+							className={size + " text-gray-400"}
 						/>
 					</div>
 				</Tooltip>
-			)
+			);
 		}
 		// If an "-" is inside "app_status", multiple lines (e.g. "Schneelastwaage")
-		if (Array.from(attr.app_status)[0] !== '-') {
-			let app_status_split = attr.app_status.split('-')
+		if (Array.from(attr.app_status)[0] !== "-") {
+			let app_status_split = attr.app_status.split("-");
 			if (app_status_split.length > 1) {
 				return (
-					<div className="flex p-2 flex-col text-sm">
+					<div className="flex flex-col p-2 text-sm">
 						{app_status_split.map((row) => (
 							<div className="text-right">{row.trim()}</div>
 						))}
 					</div>
-				)
+				);
 			}
 		}
 		// return app_status
 		return (
-			<div className="flex justify-center items-center p-2">
+			<div className="flex items-center justify-center p-2">
 				{attr.app_status}
 			</div>
-		)
-	}
+		);
+	};
 
 	render() {
 		return (
 			<>
-				<div className="flex justify-between w-full my-1">
+				<div className="my-1 flex w-full justify-between">
 					<div className="truncate">
 						<div className="w-full truncate font-bold">
 							{this.props.device.serial}
 						</div>
-						<div className="text-sm italic truncate pr-0.5">
+						<div className="truncate pr-0.5 text-sm italic">
 							{this.lastData()}
 						</div>
-						<div className="text-sm italic truncate pr-0.5">
+						<div className="truncate pr-0.5 text-sm italic">
 							{this.additionalData(this.attr, this.props.device)}
 						</div>
 					</div>
@@ -154,6 +162,6 @@ export default class DeviceRow extends Component {
 
 				<hr />
 			</>
-		)
+		);
 	}
 }
