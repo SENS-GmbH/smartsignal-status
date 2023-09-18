5db89d33da286e091ed0ae756a536695d8729a90
Sebastian Schlosser <sebastian.schlosser@sens.at>
2023-09-18 22:52:36 +0200

    test qr scanner

--- src/components/test/Router.jsx
+++ src/components/test/Router.jsx
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Success from './Success.jsx'
-import Test from './Test.jsx'
+// import Test from './Test.jsx'
 
 export default class TestRouter extends Component {
+	// <Route path="/default" element={<Test />} />
 	render() {
 		return (
 			<>
@@ -14,7 +15,6 @@ export default class TestRouter extends Component {
 						exact
 						element={<Navigate to="./default" replace />}
 					/>
-					<Route path="/default" element={<Test />} />
 					<Route path="/success/:addDevice" element={<Success />} />
 					<Route path="*" element={<Navigate to="./" replace />} />
 				</Routes>
